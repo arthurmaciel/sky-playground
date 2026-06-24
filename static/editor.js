0b4fc9c1ea63d24da183f89c3ccd982619529feb
Arthur Maciel <arthurmaciel@gmail.com>
2026-06-24 12:39:21 -0300

    fix(UI) MutationObserver tracks dropdown changes

--- static/editor.js
+++ static/editor.js
@@ -1,13 +1,57 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/elm");
+editor.setFontSize(16);
 editor.textInput.getElement().id = "editor-textarea";
 editor.textInput.getElement().name = "editor-textarea";
 
-// codeMirror.value = editor.getValue();
+initialCode = "-- Insert you code bellow or select an example above";
+editor.setValue(initialCode, -1);
 
-editor.getSession().on("change", function() {
-    var codeMirror = document.getElementById('code-mirror');
-    codeMirror.value = editor.getValue();
-    console.log(codeMirror.value);
+var aceMirror = document.getElementById('ace-mirror');
+var exampleMirror = document.getElementById('example-mirror');
+var exampleMirrorWrapper = document.getElementById('example-mirror-wrapper');
+var themeSelectorWrapper = document.getElementById('theme-selector-wrapper');
+var theme = document.getElementById('theme');
+
+// Sync Ace editor and example-mirror
+editor.session.on("change", () => {
+  aceMirror.value = editor.getValue();
 });
+
+// Trigger initial sync
+editor.getSession()._emit('change',
+  {
+    start: { row: 0, column: 0 },
+    end: { row: 0, column: 0 },
+    action: 'insert',
+    lines: []
+  }
+);
+
+// Apparently SSE diffs don't trigger JS 'change' events, 
+// so MutationObserver is needed
+var dropDownObserver = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    console.log(`Mutation type: ${mutation.type}`);
+    if (mutation.target.id === exampleMirrorWrapper.id) {
+      editor.setValue(exampleMirror.value, -1);
+    } else if (mutation.target.id === themeSelectorWrapper.id) {
+      editor.setTheme("ace/theme/chrome");
+    } else {
+      console.log(`Mutation target: ${mutation.target.id}`);
+    }
+  });
+});
+
+dropDownObserver.observe(exampleMirrorWrapper, {
+  attributes: true,
+});
+
+dropDownObserver.observe(themeSelectorWrapper, {
+  childList: true,
+  attributes: true,
+  subtree: true,       // Only watch direct children (set true for all descendants)
+  characterData: true
+});
+
